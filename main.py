879d61813fc300dadd9b67dd361addee72bcd5c9
Carlos M.I <karritos@live.com>
2019-10-09 11:52:50 -0500

    Error 500 handler added

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ app = Flask(__name__)
 def not_found(error):
     return render_template('404.html', error=error)
 
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html', error=error)
+
+
 @app.route('/')
 def index():
     return 'hello from here :)ss'
@@ -15,10 +21,15 @@ def index():
 @app.route('/hello')
 def hello():
     user_ip = request.remote_addr
-    
+
     context = {
         'todo_list': todos,
         'user_ip': user_ip,
     }
 
     return render_template('hello.html', **context)
+
+#emule error 500.
+@app.route('/error')
+def error():
+    return 1 / 0
