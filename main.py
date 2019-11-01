e1f297d223a0cf59a9e267a921045daf78b53046
Carlos M.I <karritos@live.com>
2019-11-01 16:55:41 -0600

    use Session

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
 
 todos = ['Compra', 'Ejercicio', 'trabajar', 'Dormir', 'Repetir']
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
 
 @app.errorhandler(404)
@@ -19,18 +21,20 @@ def internal_error(error):
 
 @app.route('/')
 def index():
+    user_ip = request.remote_addr
+    session['user_ip'] = user_ip
     return 'hello from here :)ss'
 
 @app.route('/hello')
 def hello():
-    user_ip = request.remote_addr
-
+    user_ip = session.get('user_ip')
     context = {
         'todo_list': todos,
         'user_ip': user_ip,
     }
 
     return render_template('hello.html', **context)
+
 
 #emule error 500.
 @app.route('/error')
