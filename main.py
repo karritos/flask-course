65398f3a24394e71ff5ac92d4934def63b638ce2
Carlos M.I <karritos@live.com>
2019-11-06 18:11:35 -0600

    flask wtf installed

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 todos = ['Compra', 'Ejercicio', 'trabajar', 'Dormir', 'Repetir']
 
@@ -25,12 +28,15 @@ def index():
     session['user_ip'] = user_ip
     return 'hello from here :)ss'
 
+
 @app.route('/hello')
 def hello():
     user_ip = session.get('user_ip')
+    login_form = LoginForm()
     context = {
         'todo_list': todos,
         'user_ip': user_ip,
+        'login_form': login_form
     }
 
     return render_template('hello.html', **context)
@@ -40,3 +46,9 @@ def hello():
 @app.route('/error')
 def error():
     return 1 / 0
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
