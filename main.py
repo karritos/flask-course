725e67e81ed7f9964f54948a85031a49dbe0e831
Carlos M.I <karritos@live.com>
2019-11-11 17:22:24 -0600

    flashes implemented

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response, redirect, render_template, session, url_for
+from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
@@ -45,7 +45,8 @@ def hello():
     if login_form.validate_on_submit():
         username = login_form.username.data
         session['username'] = username
-        return redirect(url_for('index'))
+        flash('nombre de usuario creado correctamente')
+        flash('here xD')
 
     return render_template('hello.html', **context)
 
