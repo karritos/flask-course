46af8b1b84afe16615befda921a911176a021f6b
Carlos M.I <karritos@live.com>
2019-12-30 10:27:49 -0600

    test defined

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
+import unittest
 
 todos = ['Compra', 'Ejercicio', 'trabajar', 'Dormir', 'Repetir']
 
@@ -11,6 +12,10 @@ bootstrap = Bootstrap(app)
 
 app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
+@app.cli.command()
+def test():
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner.run(tests)
 
 @app.errorhandler(404)
 def not_found(error):
